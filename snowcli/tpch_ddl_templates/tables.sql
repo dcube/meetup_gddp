85b0ba77fd19faa6b1b36c2b70aacf9b723a5f0b
papy-brossard <fbrossard@gmail.com>
2024-11-18 23:16:42 +0000

    test

--- snowcli/tpch_ddl_templates/tables.sql
+++ snowcli/tpch_ddl_templates/tables.sql
@@ -4,7 +4,7 @@
 -- create tables for tpch
 ------------------------------------------------------------------------------
 {% set domain = "MEETUP_GDDP" %}
-{% set schemas = ["TPCH_SF100", "TPCH_SF100_ICEBERG"] %}
+{% set schemas = ["TPCH_SF100", ] %}
 
 {% for schema in schemas %}
     {% set create_stmt = "CREATE TABLE" %}
