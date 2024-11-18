f3dada43f3b1fb5c0244d0a06362aae78e70548c
papy-brossard <fbrossard@gmail.com>
2024-11-18 21:54:04 +0000

    init deploy

--- .devcontainer/install/01-configure-git.sh
+++ .devcontainer/install/01-configure-git.sh
@@ -17,25 +17,7 @@ git config --global --add safe.directory "$WORKSPACE_PATH"
 git config --global core.eol lf
 git config --global core.autocrlf false
 git config --global pull.rebase true
-git config --global user.email "$def load_table(session: Session, tbl: Dict[str, str | bool]) -> None:
-    """ load table """
-    try:
-        raw_table = RawTable(
-            session=session,
-            name=str(tbl["table_name"])
-            )
-
-        raw_table.load_from_csv(
-            location=str(tbl.get("stage_path")),
-            file_format=str(tbl.get("file_format")),
-            mode=WriteMode(str(tbl.get("mode")).lower()),
-            force=bool(tbl.get("force"))
-            )
-
-        print(f"load table {str(tbl['table_name'])} succeeded")
-
-    except Exception as err:
-        print(f"load table {str(tbl['table_name'])} failed with error {err}")"
+git config --global user.email "$GIT_USEREMAIL"
 git config --global user.name "$GIT_USERNAME"
 EOF
 
