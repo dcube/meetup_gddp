87962c214b3e0c46fcec7e48771a4d275cb6c635
papy-brossard <fbrossard@gmail.com>
2024-11-20 22:00:31 +0000

    test nlitx

--- snowcli/tpch_ddl_templates/dag_nlitx_parallel.sql
+++ snowcli/tpch_ddl_templates/dag_nlitx_parallel.sql
@@ -24,7 +24,8 @@
             WAREHOUSE=ANALYSIS
             AFTER {{ domain }}.{{ schema }}.NLITX_PARALLEL_MAIN
             AS
-            EXECUTE IMMEDIATE FROM @{{ domain }}.UTILS.GIT_REPO/{{ git_ref }}/snowcli/tpch_queries/{{ basename | lower }}.sql
+            --EXECUTE IMMEDIATE FROM @{{ domain }}.UTILS.GIT_REPO/{{ git_ref }}/snowcli/tpch_queries/{{ basename | lower }}.sql
+            {{ '@'+ domain + '.UTILS.GIT_REPO/' + git_ref + '/snowcli/tpch_queries/' + basename + '.sql' | read_file }}
             ;
 
         -- Resume child tasks to enable them
